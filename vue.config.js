7f14515b42abe4e41ba51a171d169c708e00451a
Andreas_Just <andreas.wq@gmail.com>
2019-12-21 00:36:01 +0200

    dist add

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   css: {
     loaderOptions: {
-      sass: {
-        data: `@import "./src/assets/scss/variables.scss";`
-      },
+      // sass: {
+      //   data: `@import "./src/assets/scss/variables.scss";`
+      // },
     }
   },
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/movie-app/'
+    ? '/movie-app/dist/'
     : '/',
 };
