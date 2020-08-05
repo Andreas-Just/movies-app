e580844a588b9a99f44dc6354f4394238cf2500c
Andreas_Just <andreas.wq@gmail.com>
2020-08-05 19:44:37 +0300

    minor fixed

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
-const apiKey = 'api_key=04322954778d78bd6fc31bdc578dc2b0';
+const API_KEY = process.env.VUE_APP_API_KEY_MOVIEDB;
 
-const getPopularMovies = (page=1) => axios.get(`movie/popular?${apiKey}&language=en-US&page=${page}`);
-const getMovieByID = (id) => axios.get(`movie/${id}?${apiKey}`);
-const getSimilarMovies = (id) => axios.get(`movie/${id}/similar?${apiKey}&language=en-US`);
-const getSearchMovies = (newQuery) => axios.get(`search/movie?${apiKey}&language=en-US&query=${newQuery}&page=1`);
+const getPopularMovies = (page=1) => axios.get(`movie/popular?${API_KEY}&language=en-US&page=${page}`);
+const getMovieByID = (id) => axios.get(`movie/${id}?${API_KEY}`);
+const getSimilarMovies = (id) => axios.get(`movie/${id}/similar?${API_KEY}&language=en-US`);
+const getSearchMovies = (newQuery) => axios.get(`search/movie?${API_KEY}&language=en-US&query=${newQuery}&page=1`);
 
 export {
   axios,
